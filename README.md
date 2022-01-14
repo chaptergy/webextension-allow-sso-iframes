cf347a56c382b0d04e66d5ef2d778cde58b8ef7a
chaptergy <git@chaptergy.xyz>
2022-01-14 12:13:18 +0100

    Formatting

--- README.md
+++ README.md
@@ -2,14 +2,12 @@
 
 This plugin changes HTTP headers to allow websites to be iframed which block this. It is meant for development and testing purposes.
 
-
 It does the following:
 
- - Drop all `x-frame-options` response headers
- - Drop all `content-security-policy` response headers
- - Change all `set-cookie` response headers to set `SameSite=None` (this also requires the `Secure` flag to be set for the cookie)
- - Change `sec-fetch-dest` request headers to `document` if it equals `iframe`
-
+- Drop all `x-frame-options` response headers
+- Drop all `content-security-policy` response headers
+- Change all `set-cookie` response headers to set `SameSite=None` (this also requires the `Secure` flag to be set for the cookie)
+- Change `sec-fetch-dest` request headers to `document` if it equals `iframe`
 
 Note, that when cookies within this iframe are required, the iframe content has to be HTTPS, otherwise cookies will not be set.
 
--- background.js
+++ background.js
@@ -1,52 +1,60 @@
 // Change outgoing headers
 try {
   chrome.webRequest.onBeforeSendHeaders.addListener(
-    changeRequestHeaders, {
-      urls: ['<all_urls>']
-    }, ['blocking', 'requestHeaders', 'extraHeaders']
+    changeRequestHeaders,
+    {
+      urls: ['<all_urls>'],
+    },
+    ['blocking', 'requestHeaders', 'extraHeaders']
   );
-} catch(e) {
+} catch (e) {
   // On firefox 'extraHeaders' is included in 'requestHeaders' and can't be set explicitly
   chrome.webRequest.onBeforeSendHeaders.addListener(
-    changeRequestHeaders, {
-      urls: ['<all_urls>']
-    }, ['blocking', 'requestHeaders']
+    changeRequestHeaders,
+    {
+      urls: ['<all_urls>'],
+    },
+    ['blocking', 'requestHeaders']
   );
 }
 
 // Change incoming headers
 try {
   chrome.webRequest.onHeadersReceived.addListener(
-    changeResponseHeaders, {
-      urls: ["<all_urls>"]
-    }, ["blocking", "responseHeaders", "extraHeaders"]
+    changeResponseHeaders,
+    {
+      urls: ['<all_urls>'],
+    },
+    ['blocking', 'responseHeaders', 'extraHeaders']
   );
-} catch(e) {
+} catch (e) {
   // On firefox 'extraHeaders' is included in 'requestHeaders' and can't be set explicitly
   chrome.webRequest.onHeadersReceived.addListener(
-    changeResponseHeaders, {
-      urls: ["<all_urls>"]
-    }, ["blocking", "responseHeaders"]
+    changeResponseHeaders,
+    {
+      urls: ['<all_urls>'],
+    },
+    ['blocking', 'responseHeaders']
   );
 }
 
-function changeResponseHeaders(details){
-  if(details.responseHeaders.length > 0){
+function changeResponseHeaders(details) {
+  if (details.responseHeaders.length > 0) {
     for (var i = details.responseHeaders.length - 1; i >= 0; --i) {
       var header = details.responseHeaders[i];
-      console.log("Header", i, header.name);
+      console.log('Header', i, header.name);
       if (header.name.toLowerCase() === 'x-frame-options' || header.name.toLowerCase() === 'content-security-policy') {
         // Remove x-frame-options and content-security-policy
         details.responseHeaders.splice(i, 1);
       } else if (header.name.toLowerCase() === 'set-cookie') {
         // Change cookies to be SameSite=None
-        if(header.value.match(/;\s*[Ss]ame[Ss]ite=/)){
+        if (header.value.match(/;\s*[Ss]ame[Ss]ite=/)) {
           header.value = header.value.replace(/;\s*[Ss]ame[Ss]ite=[^;]*/, '; SameSite=None');
         } else {
           header.value += '; SameSite=None';
         }
         // Set secure flag if not there (Required for SameSite)
-        if(!header.value.match(/;\s*[Ss]ecure/)){
+        if (!header.value.match(/;\s*[Ss]ecure/)) {
           header.value += '; Secure';
         }
       }
@@ -55,9 +63,12 @@ function changeResponseHeaders(details){
   }
 }
 
-function changeRequestHeaders(details){
+function changeRequestHeaders(details) {
   for (var i = 0; i < details.requestHeaders.length; ++i) {
-    if (details.requestHeaders[i].name.toLowerCase() === 'sec-fetch-dest' && details.requestHeaders[i].value.toLowerCase() === 'iframe') {
+    if (
+      details.requestHeaders[i].name.toLowerCase() === 'sec-fetch-dest' &&
+      details.requestHeaders[i].value.toLowerCase() === 'iframe'
+    ) {
       details.requestHeaders[i].value = 'document';
       break;
     }
